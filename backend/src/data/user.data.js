5f6c528dc2983367c680036a06a46ff355acf15f
DungBeu78 <ilovesingaporebest@gmail.com>
2026-04-22 13:25:03 +0700

    Add initial user details

--- backend/src/data/user.data.js
+++ backend/src/data/user.data.js
@@ -1,11 +1,17 @@
 // backend/src/data/user.data.js
 
+import { use } from "react";
+
 // In-memory user storage
 const users = new Map();
 
 // Add initial users (optional)
-users.set("1", { id: "1", username: "2452712", password: "123", role: "learner", cardActive: true });
-users.set("2", { id: "2", username: "fstaff", password: "123", role: "staff" });
+users.set("1", { id : "1", username: "2451111", password: "123", role: "learner", cardActive: false, IDstatus: true });
+users.set("2", { id : "2", username: "2452222", password: "123", role: "staff", cardActive: false, IDstatus: true });
+users.set("3", { id : "3", username: "2453333", password: "123", role: "guest", cardActive: false});
+users.set("4", { id : "4", username: "2454444", password: "123", role: "parkingOperator", cardActive: false});
+users.set("5", { id : "5", username: "2455555", password: "123", role: "systemAdministrators", cardActive: false});
+users.set("6", { id : "6", username: "2456666", password: "123", role: "BKPayIntegration", cardActive: false});
 
 // Utility functions
 export const addUser = (user) => {
